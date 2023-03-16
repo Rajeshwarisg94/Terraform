e2393996c45a04783609104cf8eced47c135ba8b
rekha <rekha946@gmail.com>
2023-03-16 16:14:01 +0530

    change 4

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "myFirstInstance" {
-  ami           = "ami-030cf0a1edb8636ab"
+  ami           = var.ami_id
   key_name = "Rekha"
   instance_type = "t2.micro"
   tags= {
---
+++ variable.tf
@@ -0,0 +1,4 @@
+variable "ami_id" {
+    type = string
+    default = "ami-0f8ca728008ff5af4"
+}
