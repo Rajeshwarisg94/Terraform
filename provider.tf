8b76db02509cde1953c89485d1d35078684b7635
rekha <rekha946@gmail.com>
2023-03-16 17:35:49 +0530

    changes 7

--- provider.tf
+++ provider.tf
@@ -4,11 +4,26 @@ provider "aws" {
     region = "ap-northeast-1"
 }
 
+variable "ami_id" {
+    description = "ID of AMI to use for the instance"
+    type = string
+    default = "ami-030cf0a1edb8636ab"
+}
+
 resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami_id
+  ami = var.ami_id
   key_name = "Rekha"
   instance_type = "t2.micro"
   tags= {
-    Name = "terraform_ec2.1"
+    Name = "my_ec2"
+  }
+}
+
+resource "aws_instance" "mysecondInstance" {
+  ami = var.ami_id
+  key_name = "Rekha"
+  instance_type = "t2.micro"
+  tags= {
+    Name = "my_ec2.2"
   }
 }
--- variable.tf
+++
@@ -1,4 +0,0 @@
-variable "ami_id" {
-    type = string
-    default = "ami-0f8ca728008ff5af4"
-}
